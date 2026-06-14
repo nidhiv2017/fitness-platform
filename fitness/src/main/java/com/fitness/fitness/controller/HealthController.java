d7575993adeda6a2b794b6e583d257c697422966
nidhiv2017 <116021984+nidhiv2017@users.noreply.github.com>
2026-06-14 08:47:08 +0000

    for health api initiated

---
+++ fitness/src/main/java/com/fitness/fitness/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.fitness.fitness.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "Fitness Backend Running";
+    }
+}
